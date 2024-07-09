dc9df04a4dde3c56c312a30418c190e1baf32508
falsystack <falsystack@gmail.com>
2024-07-09 22:26:04 +0900

    test: Test with JUnit5ˆ

--- src/test/java/jp/falsystack/cafekiosk/unit/CafekioskTest.java
+++ src/test/java/jp/falsystack/cafekiosk/unit/CafekioskTest.java
@@ -1,18 +1,55 @@
 package jp.falsystack.cafekiosk.unit;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import jp.falsystack.cafekiosk.unit.beverages.Americano;
+import jp.falsystack.cafekiosk.unit.beverages.Latte;
 import org.junit.jupiter.api.Test;
 
 class CafekioskTest {
+
+  @Test
+  void add_manual_test() {
+    Cafekiosk cafekiosk = new Cafekiosk();
+    cafekiosk.add(new Americano());
+
+    System.out.println(">>> 담긴 음료수 : " + cafekiosk.getBeverages());
+    System.out.println(">>> 담긴 음료 : " + cafekiosk.getBeverages().get(0).getName());
+  }
 
   @Test
   void add() {
     Cafekiosk cafekiosk = new Cafekiosk();
     cafekiosk.add(new Americano());
 
-    System.out.println(">>> 담긴 음료수 : " + cafekiosk.getBeverages());
-    System.out.println(">>> 담긴 음료 : " + cafekiosk.getBeverages().get(0).getName());
+    assertThat(cafekiosk.getBeverages()).hasSize(1);
+    assertThat(cafekiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+  }
+
+  @Test
+  void remove() {
+    var cafekiosk = new Cafekiosk();
+    var americano = new Americano();
+
+    cafekiosk.add(americano);
+    assertThat(cafekiosk.getBeverages()).hasSize(1);
+
+    cafekiosk.remove(americano);
+    assertThat(cafekiosk.getBeverages()).isEmpty();
+  }
+
+  @Test
+  void clear() {
+    var cafekiosk = new Cafekiosk();
+    var americano = new Americano();
+    var latte = new Latte();
+
+    cafekiosk.add(americano);
+    cafekiosk.add(latte);
+    assertThat(cafekiosk.getBeverages()).hasSize(2);
+
+    cafekiosk.clear();
+    assertThat(cafekiosk.getBeverages()).isEmpty();
   }
 }
---
+++ src/test/java/jp/falsystack/cafekiosk/unit/beverages/AmericanoTest.java
@@ -0,0 +1,24 @@
+package jp.falsystack.cafekiosk.unit.beverages;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AmericanoTest {
+
+  @Test
+  void getName() {
+    Americano americano = new Americano();
+
+    assertEquals("아메리카노", americano.getName());
+    assertThat(americano.getName()).isEqualTo("아메리카노");
+  }
+
+  @Test
+  void getPrice() {
+    Americano americano = new Americano();
+
+    assertThat(americano.getPrice()).isEqualTo(4000);
+  }
+}
